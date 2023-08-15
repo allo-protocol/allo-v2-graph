6121bbee738734af8f79f3fb2cf66f6f29ea6abc
Jaxcoder <codenamejason@outlook.com>
2023-08-15 15:05:41 -0400

    chore: update with latest contracts/deploy goerli

--- src/strategies/directGrantsSimple.ts
+++ src/strategies/directGrantsSimple.ts
@@ -1,15 +1,25 @@
-// NOTE: The following types will come from generated code when we have the abi
-// for the strategy and the strategy is deployed.
-// export function handleRecipientStatusChanged(event: RecipientStatusChanged): void {}
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Allocated,
+    MilestoneStatusChanged,
+    MilestoneSubmitted,
+    MilestonesSet,
+    PoolActive,
+    RecipientStatusChanged,
+    Registered
+} from "../../generated/DirectGrantsSimpleStrategy/DirectGrantsSimpleStrategy";
 
-// export function handleMilestoneSubmitted(event: MilestoneSubmitted): void {}
+export function handleRecipientStatusChanged(event: RecipientStatusChanged): void {}
 
-// export function handleMilestoneStatusChanged(event: MilestoneStatusChanged): void {}
+export function handleMilestoneSubmitted(event: MilestoneSubmitted): void {}
 
-// export function handleMilestonesSet(event: MilestonesSet): void {}
+export function handleMilestoneStatusChanged(event: MilestoneStatusChanged): void {}
 
-// export function handleAllocated(event: Allocated): void {}
+export function handleMilestonesSet(event: MilestonesSet): void {}
 
-// export function handleRegistered(event: Registered): void {}
+export function handleAllocated(event: Allocated): void {}
 
-// export function handlePoolActive(event: PoolActive): void {}
+export function handleRegistered(event: Registered): void {}
+
+export function handlePoolActive(event: PoolActive): void {}
--- src/strategies/donationVotingMerkle.ts
+++ src/strategies/donationVotingMerkle.ts
@@ -1,23 +1,39 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+    Allocated,
+    Appealed,
+    BatchPayoutSuccessful,
+    Claimed,
+    Distributed,
+    DistributionUpdated,
+    FundsDistributed,
+    PoolActive,
+    RecipientStatusUpdated,
+    Registered,
+    TimestampsUpdated
+} from "../../generated/DonationVotingMerkleDistributionStrategy/DonationVotingMerkleDistributionStrategy";
+
 // NOTE: The following types will come from generated code when we have the abi
 // for the strategy and the strategy is deployed.
-// export function handleAppealed(event: Appealed): void {}
+export function handleAppealed(event: Appealed): void {}
 
-// export function handleRecipientStatusUpdated(event: RecipientStatusUpdated): void {}
+export function handleRecipientStatusUpdated(event: RecipientStatusUpdated): void {}
 
-// export function handleClaimed(event: Claimed): void {}
+export function handleClaimed(event: Claimed): void {}
 
-// export function handleTimestampsUpdated(event: TimestampsUpdated): void {}
+export function handleTimestampsUpdated(event: TimestampsUpdated): void {}
 
-// export function handleDistributionUpdated(event: DistributionUpdated): void {}
+export function handleDistributionUpdated(event: DistributionUpdated): void {}
 
-// export function handleFundsDistributed(event: FundsDistributed): void {}
+export function handleFundsDistributed(event: FundsDistributed): void {}
 
-// export function handleBatchPayoutSuccessful(event: BatchPayoutSuccessful): void {}
+export function handleBatchPayoutSuccessful(event: BatchPayoutSuccessful): void {}
 
-// export function handleAllocated(event: Allocated): void {}
+export function handleAllocated(event: Allocated): void {}
 
-// export function handleDistributed(event: Distributed): void {}
+export function handleDistributed(event: Distributed): void {}
 
-// export function handlePoolActive(event: PoolActive): void {}
+export function handlePoolActive(event: PoolActive): void {}
 
-// export function handleRegistered(event: Registered): void {}
+export function handleRegistered(event: Registered): void {}
